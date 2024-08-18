087a4ba6417e8dc1542b17c62147ec36c480d3b4
arcorium <arcorium.l@gmail.com>
2024-08-18 22:16:43 +0700

    chore(customer): merge event name and the implementation into single file

--- services/customer/internal/domain/event/customer.go
+++ services/customer/internal/domain/event/customer.go
@@ -5,6 +5,25 @@ import (
   "time"
 )
 
+const (
+  CustomerCreatedEvent                    = "customer.created"
+  CustomerUpdatedEvent                    = "customer.updated"
+  CustomerStatusUpdatedEvent              = "customer.status.updated"
+  CustomerBalanceUpdatedEvent             = "customer.balance.updated"
+  CustomerPasswordUpdatedEvent            = "customer.password.updated"
+  CustomerPhotoUpdatedEvent               = "customer.avatar.updated"
+  CustomerEmailVerifiedEvent              = "customer.email.verified"
+  CustomerForgotPasswordRequestedEvent    = "customer.forgot-password.requested"
+  CustomerEmailVerificationRequestedEvent = "customer.email-verif.requested"
+  CustomerAddressAddedEvent               = "customer.address.added"
+  CustomerAddressDeletedEvent             = "customer.addresses.deleted"
+  CustomerAddressUpdatedEvent             = "customer.address.updated"
+  CustomerVoucherAddedEvent               = "customer.vouchers.added"
+  CustomerVoucherDeletedEvent             = "customer.vouchers.deleted"
+  CustomerVoucherUpdatedEvent             = "customer.voucher.updated"
+  CustomerDefaultAddressUpdatedEvent      = "customer.default-address.updated"
+)
+
 var _ types.Event = (*CustomerCreatedV1)(nil)
 
 type CustomerCreatedV1 struct {
